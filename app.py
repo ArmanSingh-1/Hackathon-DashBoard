63bf68d37809a09c5618374ca01f34e3cc8461d0
ArmanSingh-1 <Armansinghbhadoriya@gmail.com>
2025-06-23 19:37:23 +0530

    Integration of Data_Visualizer and app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from Data_Visualizer import generate_graph
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    generate_graph()
     return render_template('index.html')
 
 if __name__ == '__main__':
