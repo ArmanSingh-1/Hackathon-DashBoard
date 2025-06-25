40437a05963a06d48d443f899a9ff0a47b238bda
ArmanSingh-1 <Armansinghbhadoriya@gmail.com>
2025-06-25 17:15:49 +0530

    Updated app

--- app.py
+++ app.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    generate_graph()
     return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('index.html')
+    generate_graph()
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
